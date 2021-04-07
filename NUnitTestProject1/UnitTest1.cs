ac93c7b723129b6d94830c664ce5802036295b59
akshayraut3105 <akshayraut3105@gmail.com>
2021-04-07 12:06:10 +0530

    [AKSHAY] added method for invoice of premium rides

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -52,5 +52,43 @@ namespace NUnitTestProject
             // Assert
             Assert.AreEqual(expectedInvoiceSummary, invoiceSummary);
         }
+        // TC.4- GivenUserId_ShouldReturnInvoiceSummary
+        [Test]
+        public void GivenUserId_ShouldReturnInvoiceSummary()
+        {
+            // Arrange 
+            // initialising the object of the invoice generator class
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            // Creating the object of the ride repository
+            RideRepository repository = new RideRepository();
+            // Initialising the user ID
+            string userID = "akash";
+            // Initialising the ride array with details of the ride
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            // Adding the ride data for the user to the ride repository
+            repository.AddRide(userID, rides);
+            // Getting the ride data from the ride repository class using the user ID
+            Ride[] rideData = repository.GetRides(userID);
+            // Getting the invoice summary when passing the ride Data
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateAvgFare(rideData);
+            // Expected value of the Invoice Summary
+            InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(30.0, 2, 15.0);
+            // Asserting the comparison between the expected and actual invoice summary
+            Assert.AreEqual(expectedInvoiceSummary, invoiceSummary);
+        }
+        // TC.5-GeneratesInvoiceForRideType-Premium Ride
+        [Test]
+        public void GeneratesInvoiceForRideType()
+        {
+            double distance = 2.0;
+            int time = 5;
+            // Initializing the object with normal ride type
+            invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            // Invoking the Calculate Fare method to get the total actual fare
+            double totalActualFare = invoiceGenerator.CalculateFare(distance, time);
+            double totalExpectedFare = 40.0;
+            // Asserting with the expected value
+            Assert.AreEqual(totalExpectedFare, totalActualFare);
+        }
     }
 }
